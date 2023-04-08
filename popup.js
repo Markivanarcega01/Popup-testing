5569cc48a0235164c76de1d76bc9258755c095ef
Markivanarcega01 <arcegamarkivan@gmail.com>
2023-04-08 22:59:36 +0800

    first commit

---
+++ popup.js
@@ -0,0 +1,18 @@
+var modalbtns = document.querySelectorAll('.modal-open');
+
+modalbtns.forEach(function(btn){
+    btn.onclick= function(){
+        var modal = btn.getAttribute('data-modal');
+
+        document.getElementById(modal).style.display = 'block';
+
+    };
+});
+
+var closebtns = document.querySelectorAll('.modal-close');
+
+closebtns.forEach(function(btn){
+    btn.onclick  = function(){
+        var modal  = btn.closest('.modal').style.display = 'none';
+    }
+});
